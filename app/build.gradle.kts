b776496423da555b751aeb07cc8fdb45424eb905
WorldOneTop <dlwpdlf147@naver.com>
2023-11-28 19:15:07 +0900

    버전 카탈로그 마이그레이션 및 Safe Args 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
-    id("com.google.devtools.ksp")
-    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.agp)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.navigation.safeargs)
 }
 
 android {
@@ -36,75 +37,41 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+//    kotlin {
+//        jvmToolchain(8)
+//    }
+    viewBinding{
+        enable = true
+    }
+    dataBinding{
+        enable = true
+    }
 }
 
 dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.bundles.coroutine)
+    implementation(libs.activity)
+    implementation(libs.fragment)
 
-    val coroutineVersion = "1.7.2"
-    val hiltVersion = "2.48"
-    val lifecycleVersion = "2.6.2"
-    val pagingVersion = "3.2.1"
-    val navigationVersion= "2.7.5"
-    val retrofitVersion = "2.9.0"
-    val roomVersion = "2.6.0"
-    val glideVersion ="4.16.0"
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 
-
-    // android coroutine
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
-
-    // kotlin view
-    implementation("androidx.activity:activity-ktx:1.8.1")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-
-    // hilt
-    implementation("com.google.dagger:hilt-android:$hiltVersion")
-    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
-
-    // ktx
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-
-    // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
-    // ProcessLifecycleOwner provides a lifecycle for the whole application process
-    implementation("androidx.lifecycle:lifecycle-process:$lifecycleVersion")
-
-    // paging
-    implementation("androidx.paging:paging-runtime-ktx:$pagingVersion")
-
-    // navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
-    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
-
-    // retrofit
-    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
-    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
-    implementation("com.squareup.retrofit2:converter-scalars:$retrofitVersion")
-    // gson
-    implementation("com.google.code.gson:gson:$retrofitVersion")
-    // logging
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.2")
-
-    // image
-    implementation("com.github.bumptech.glide:glide:$glideVersion")
-    annotationProcessor("com.github.bumptech.glide:compiler:$glideVersion")
-
-    // room
-    implementation("androidx.room:room-ktx:$roomVersion")
-    implementation("androidx.room:room-runtime:$roomVersion")
-    implementation("androidx.room:room-paging:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
-
-    // EncryptedSharedPreference
-    implementation("androidx.security:security-crypto:1.0.0")
+    implementation(libs.paging)
+    implementation(libs.bundles.retrofit)
+    implementation(libs.bundles.room)
+    ksp(libs.room.compiler)
+    implementation(libs.bundles.navigation)
+    implementation(libs.security.crypto)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.glide)
+    annotationProcessor(libs.glide.compiler)
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath(libs.navigation.safeargs)
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    alias(libs.plugins.agp) apply false
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.navigation.safeargs) apply false
 }
