1ee461c72b12529a3b768a6fe05afa8edddb4473
WorldOneTop <dlwpdlf147@naver.com>
2023-12-10 01:55:47 +0900

    add: hilt init

---
+++ app/src/main/java/com/worldonetop/ourry/OurryApplication.kt
@@ -0,0 +1,8 @@
+package com.worldonetop.ourry
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OurryApplication: Application() {
+}
--- app/src/main/java/com/worldonetop/ourry/ui/main/OurryActivity.kt
+++ app/src/main/java/com/worldonetop/ourry/ui/main/OurryActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.worldonetop.ourry.ui.theme.MyApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class OurryActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
