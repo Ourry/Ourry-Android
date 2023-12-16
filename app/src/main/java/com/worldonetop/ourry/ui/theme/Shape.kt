e178f81d6c8b5267912521596f203397ea580d97
WorldOneTop <dlwpdlf147@naver.com>
2023-12-16 18:15:27 +0900

    add: add resource and rename

--- app/src/main/java/com/worldonetop/ourry/ui/theme/Shape.kt
+++ app/src/main/java/com/worldonetop/ourry/ui/theme/Shape.kt
@@ -7,10 +7,36 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 
-val BodyTextStyle = TextStyle(
+val DefaultTextStyle = TextStyle(
     fontFamily = DefaultFont,
     fontSize = 16.sp,
     color = Color.Black,
     fontWeight = FontWeight.Normal,
     fontStyle = FontStyle.Normal,
+)
+
+
+val TitleTextStyle = DefaultTextStyle.copy(
+    fontSize = 24.sp,
+    fontWeight = FontWeight.Bold,
+    lineHeight = (36*1.1).sp
+)
+
+val BodyTextStyle = DefaultTextStyle.copy(
+    fontWeight = FontWeight.SemiBold,
+)
+
+val PlaceholderTextStyle = BodyTextStyle.copy(
+    color = Gray30
+)
+
+val ButtonTextStyle = BodyTextStyle.copy(
+    color = BackgroundPrimaryColor,
+    fontWeight = FontWeight.Bold,
+)
+
+val ErrorTextStyle = DefaultTextStyle.copy(
+    color = ErrorColor,
+    fontSize = 12.sp,
+    fontWeight = FontWeight.SemiBold,
 )
