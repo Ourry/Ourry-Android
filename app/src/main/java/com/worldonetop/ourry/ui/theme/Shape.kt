09ac6282f22e3c23b2ae0a26cb69ffdbc926466d
WorldOneTop <dlwpdlf147@naver.com>
2024-01-07 05:09:09 +0900

    add: add resource

--- app/src/main/java/com/worldonetop/ourry/ui/theme/Shape.kt
+++ app/src/main/java/com/worldonetop/ourry/ui/theme/Shape.kt
@@ -11,8 +11,9 @@ val DefaultTextStyle = TextStyle(
     fontFamily = DefaultFont,
     fontSize = 16.sp,
     color = Color.Black,
-    fontWeight = FontWeight.Normal,
+    fontWeight = FontWeight.Medium,
     fontStyle = FontStyle.Normal,
+    lineHeight = (16*1.1).sp
 )
 
 
@@ -22,8 +23,26 @@ val TitleTextStyle = DefaultTextStyle.copy(
     lineHeight = (36*1.1).sp
 )
 
+val LargeTextStyle = DefaultTextStyle.copy(
+    fontWeight = FontWeight.Bold,
+    fontSize = 18.sp,
+    lineHeight = (18*1.1).sp
+)
+
 val BodyTextStyle = DefaultTextStyle.copy(
     fontWeight = FontWeight.SemiBold,
+)
+
+val SmallTextStyle = DefaultTextStyle.copy(
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp,
+    lineHeight = (14*1.1).sp
+)
+
+val TinyTextStyle = DefaultTextStyle.copy(
+    fontWeight = FontWeight.Light,
+    fontSize = 11.sp,
+    lineHeight = (11*1.1).sp
 )
 
 val PlaceholderTextStyle = BodyTextStyle.copy(
@@ -39,4 +58,5 @@ val ErrorTextStyle = DefaultTextStyle.copy(
     color = ErrorColor,
     fontSize = 12.sp,
     fontWeight = FontWeight.SemiBold,
+    lineHeight = (12*1.1).sp
 )
